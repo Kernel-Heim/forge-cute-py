af5f7ec572033e1e939809fcc04ab199f533163f
jonah <jsamost@gmail.com>
2026-01-29 16:26:50 -0800

    reduce sum 2d both dimensions

--- forge_cute_py/kernels/reduce_sum.py
+++ forge_cute_py/kernels/reduce_sum.py
@@ -1,3 +1,4 @@
+from concurrent.futures import thread
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['CUTLASS_CUDA_ARCH'] = '86'
@@ -15,35 +16,6 @@ from cutlass._mlir.dialects.nvvm import AtomicOpKind, MemOrderKind, MemScopeKind
 from cutlass.base_dsl.typing import T
 
 
-@cute.kernel
-def reduce_sum_kernel_one(input: cute.Tensor, output: cute.Tensor, N:cute.Int32, num_warps: int, max_iters: int):
-    smem_alloc = cutlass.utils.SmemAllocator()
-    smem_layout = cute.make_layout((32,))
-    shmem = smem_alloc.allocate_tensor(cute.Float32, smem_layout)
-    
-    tidx, _, _ = cute.arch.thread_idx()
-    bidx, _, _ = cute.arch.block_idx()
-    bdim, _, _ = cute.arch.block_dim()
-    lane_idx = cute.arch.lane_idx()
-    warp_idx = cute.arch.warp_idx()
-    idx = bdim * bidx + tidx
-
-    acc = cute.Float32(0)
-    for i in range(max_iters):
-        idx = idx + i * bdim
-        if idx < N:
-            acc = acc + input[idx]
-    acc = cute.arch.warp_reduction_sum(acc) 
-    if lane_idx == 0:
-        shmem[warp_idx] = acc
-    cute.arch.sync_threads()
-    if warp_idx == 0:
-        acc = shmem[lane_idx] if lane_idx < num_warps else 0.0
-        acc = cute.arch.warp_reduction_sum(acc) 
-        if lane_idx == 0:
-            output[bidx] = acc
-
-
 @dsl_user_op
 def atomicAddF32(dst_ptr: cute.Pointer, val: cute.Float32, loc=None, ip=None) -> cute.Float32:
     return nvvm.atomicrmw(
@@ -57,24 +29,27 @@ def atomicAddF32(dst_ptr: cute.Pointer, val: cute.Float32, loc=None, ip=None) ->
         ip=ip,
     )
 
+
 @cute.kernel
-def reduce_sum_kernel_atomic(input: cute.Tensor, output: cute.Tensor, N:cute.Int32, num_warps: int, coarsen: int):
+def reduce_sum_kernel_last(input: cute.Tensor, output: cute.Tensor, num_warps: int):
     smem_alloc = cutlass.utils.SmemAllocator()
     smem_layout = cute.make_layout((32,))
     shmem = smem_alloc.allocate_tensor(cute.Float32, smem_layout)
     
+    _, N = input.shape
     tidx, _, _ = cute.arch.thread_idx()
     bidx, _, _ = cute.arch.block_idx()
-    bdim, _, _ = cute.arch.block_dim()
+    bdimx, _, _ = cute.arch.block_dim()
     lane_idx = cute.arch.lane_idx()
     warp_idx = cute.arch.warp_idx()
-    base_idx = coarsen * bdim * bidx + tidx
+
+    max_iters = cute.ceil_div(N, bdimx)
 
     acc = cute.Float32(0)
-    for i in range(coarsen):
-        idx = base_idx + i * bdim
+    for i in range(max_iters):
+        idx = tidx + i * bdimx
         if idx < N:
-            acc = acc + input[idx]
+            acc = acc + input[bidx, idx]
     acc = cute.arch.warp_reduction_sum(acc) 
     if lane_idx == 0:
         shmem[warp_idx] = acc
@@ -83,29 +58,83 @@ def reduce_sum_kernel_atomic(input: cute.Tensor, output: cute.Tensor, N:cute.Int
         acc = shmem[lane_idx] if lane_idx < num_warps else 0.0
         acc = cute.arch.warp_reduction_sum(acc) 
         if lane_idx == 0:
-            atomicAddF32(output.iterator, acc)
+            output[bidx] = acc
+
+
+@cute.kernel
+def reduce_sum_kernel_first(input: cute.Tensor, output: cute.Tensor, stride: int):
+    smem_alloc = cutlass.utils.SmemAllocator()
+    smem_layout = cute.make_layout((4, 32))
+    shmem = smem_alloc.allocate_tensor(cute.Float32, smem_layout)
+    
+    M, N = input.shape
+    tidx, _, _ = cute.arch.thread_idx()
+    bidx, _, _ = cute.arch.block_idx()
+    lane_idx = cute.arch.lane_idx()
+    warp_idx = cute.arch.warp_idx()
+
+    max_iters = cute.ceil_div(M, stride)
+    col_offset = tidx % 4 
+    row_offset = tidx // 4
+    col = 4 * bidx + col_offset
+    acc = cute.Float32(0)
+
+    row = row_offset
+    for _ in range(max_iters):
+        if row < M and col < N:
+            acc = acc + input[row, col]
+        row = row + 32
+    
+    shmem[col_offset, row_offset] = acc
+    cute.arch.sync_threads()
+    acc = shmem[warp_idx, lane_idx]
+
+    acc = cute.arch.warp_reduction_sum(acc) 
+    if lane_idx == 0:
+        output[bidx * 4 + warp_idx] = acc
 
 
 @cute.jit
-def solve(input: cute.Tensor, output: cute.Tensor, N: cute.Int32):
-    # if N <= 128:
-    #     reduce_sum_kernel_one( input, output, N, 4, 1
-    #     ).launch( grid=(1, 1, 1), block=(128, 1, 1))
-    # elif N <= 10240:
-    #     reduce_sum_kernel_one( input, output, N, 32, 10
-    #     ).launch( grid=(1, 1, 1), block=(1024, 1, 1))
-    # else:
-    num_warps = 8
+def _reduce_sum_last(x, output):
+    num_warps = 4
     threads_per_block = 32 * num_warps
-    coarsen = 8
-    blocks = cute.ceil_div(N, threads_per_block * coarsen)
-    reduce_sum_kernel_atomic(input, output, N, num_warps, coarsen
-    ).launch( grid=(blocks, 1, 1), block=(threads_per_block, 1, 1))
+    m, _ = x.shape
+    reduce_sum_kernel_last(x, output, num_warps
+    ).launch( grid=(m, 1, 1), block=(threads_per_block, 1, 1))
 
 
-N = 100000
-a = torch.randn((N,), device="cuda", dtype=torch.float32)
-b = torch.zeros((1,), device='cuda', dtype=torch.float32)
-vadd_compiled = cute.compile(solve, from_dlpack(a), from_dlpack(b), N)
-vadd_compiled(from_dlpack(a), from_dlpack(b), N)
-print(f'b=={b}')
+@cute.jit
+def _reduce_sum_first(x, output):
+    num_warps = 4
+    threads_per_block = num_warps * 32
+    m, n = x.shape
+    yblocks = cute.ceil_div(n, 4)
+    reduce_sum_kernel_first(x, output, threads_per_block // 4
+    ).launch(
+        grid=(yblocks, 1, 1),
+        block=(threads_per_block, 1, 1)
+    )
+
+
+def reduce_sum(x, dim=-1):
+    shape = list(x.shape)
+    shape.pop(dim)
+    output = torch.zeros(shape, device=x.device, dtype=x.dtype)
+    if dim == -1:
+        vadd_compiled = cute.compile(_reduce_sum_last, from_dlpack(x), from_dlpack(output))
+        vadd_compiled(from_dlpack(x), from_dlpack(output))
+    else:
+        vadd_compiled = cute.compile(_reduce_sum_first, from_dlpack(x), from_dlpack(output))
+        vadd_compiled(from_dlpack(x), from_dlpack(output))
+
+    return output
+
+
+def test():
+    for dim in [-1, 0]:
+        M, N = 1100, 1200
+        a = torch.randn((M, N), device="cuda", dtype=torch.float32)
+        output = reduce_sum(a, dim=dim)
+        close = torch.allclose(output, a.sum(dim), rtol=1e-3)
+        assert close, f"Error along dimension: {dim}"
+    print("tests pass")
